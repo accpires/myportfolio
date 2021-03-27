f94112941083a6f950fb9b64cf10067df8f08673
accpires <81449446+accpires@users.noreply.github.com>
2021-03-27 09:45:28 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ def html_page(page_name):
     return render_template(page_name)
 
 def write_to_file(data):
-    with open('database.txt', mode='a') as database:
+	with open('database.txt', mode='a') as database:
 		email = data['email']
 		subject = data['subject']
 		message = data['message']
